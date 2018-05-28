547f64343897da66f7ec23c1f5cb276f76ef7bc9
derrick b <dbozich1@gmail.com>
2018-05-28 01:58:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season][holiday_name].push(supply_array)
+  holiday_hash[season][holiday_name]= supply_array
+  holiday_hash
   # code here
   # remember to return the updated hash
 
@@ -102,3 +103,5 @@ holiday_hash = {
     :memorial_day => ["BBQ"]
   }
 }
+
+add_new_holiday_with_supplies(holiday_hash, :winter, :christmas, ["wine"])
